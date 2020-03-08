7b049f8f1e806540d31116b398c47aaf57cff9a7
Pavlo Shepitchak <pshepitchak@intellias.com>
2020-03-08 18:43:20 +0200

    CompanyProfile.js component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 import CompanyProfile from './CompanyProfile';
+import View from './View';
 
 function App() {
   return (
-   <CompanyProfile/>
+    <RootView>
+      <CompanyProfile />
+    </RootView>
   );
 }
 
 export default App;
+
+const RootView = styled(View)`
+  flex:1;
+  flex-direction: column;
+  align-items: center;
+`;
--- src/CompanyProfile.js
+++ src/CompanyProfile.js
@@ -1,5 +1,56 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import googleImage from './google.jpeg';
+import Image from './Image';
+import View from './View';
 
-const CompanyProfile = () => <div>'profile'</div>;
+const profileData = {
+  name: 'Google',
+  logo: googleImage,
+  description:
+    'Google LLC is an American multinational technology company that ' +
+    'specializes in Internet-related services and products, ' +
+    'which include online advertising technologies, search engine, ' +
+    'cloud computing, software, and hardware.',
+};
+
+const CompanyProfile = ({ profile = profileData }) => (
+  <RootView>
+    <Logo src={profile.logo} />
+    <Header>{profile.name}</Header>
+    <Body>{profile.description}</Body>
+  </RootView>
+);
 
 export default CompanyProfile;
+
+CompanyProfile.propTypes = {
+  profile: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    logo: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+const RootView = styled(View)`
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  max-width: 400px;
+  border: 1px solid black;
+  padding: 12px;
+`;
+
+const Header = styled(View)`
+  font-weight: bold;
+  font-size: 22px;
+  margin: 12px 0;
+`;
+
+const Body = styled(View)``;
+
+const Logo = styled(Image)`
+  width: 100%;
+  height: auto;
+`;
