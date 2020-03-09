eb78fa165cad852360f941fb4c6c7c69fef8e72a
Pavlo Shepitchak <pshepitchak@intellias.com>
2020-03-09 11:26:17 +0200

    Updates

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as CompanyProfile } from './CompanyProfile'
-export { default as Image } from './Image'
-export { default as TabBar } from './TabBar'
-export { default as View } from './View'
